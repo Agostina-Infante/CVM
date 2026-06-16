26b1a859905065fbc2b15bbbfc978033e29a7b3f
Agostina-Infante <ainfante@obralapiedad.com.ar>
2026-06-16 11:52:23 -0300

    Add files via upload

--- JS/nosocios.js
+++ JS/nosocios.js
@@ -1,14 +1,32 @@
 const BtnSalud=document.getElementById("Salud");
 const BtnMasInfo=document.getElementById("MasInfo");
-const ContScreen=document.getElementById("Screen");
+const ContScreen01=document.getElementById("Screen01");
+const BtnImprimirOrden=document.getElementById("ImprimirOrden");
+const TxtComoContinuar=document.getElementById("ComoContinuar");
+const ContScreen02=document.getElementById("Screen02");
+const BtnSalir=document.getElementById("Salir");
+const TxtGuia=document.getElementById("Guia");
 
 BtnSalud.addEventListener('click', () => {
-    
+    MostrarScreen();
 });
 BtnMasInfo.addEventListener('click', ()=> {
+    MostrarScreen();
+});
+
+BtnImprimirOrden.addEventListener('click', () => {
+    CambiarScreen();
+});
+BtnSalir.addEventListener('click', ()=>{
     
 });
 
 function MostrarScreen(){
-    ContScreen.style.visibility="visible";
+    ContScreen01.style.visibility="visible";
 }
+function CambiarScreen(){
+    ContScreen01.style.visibility="hidden";
+    ContScreen02.style.visibility="visible";
+}
+
+fetch
--- JS/socios.js
+++ JS/socios.js
@@ -1,13 +1,34 @@
 const BtnAdmin=document.getElementById("BtnSuperior");
 const BtnDeportes=document.getElementById("BtnInferiorIz");
 const BtnSalud=document.getElementById("BtnInferiorDe");
+const ContScreen01=document.getElementById("Screen01");
+const BtnImprimirOrden=document.getElementById("ImprimirOrden");
+const TxtComoContinuar=document.getElementById("ComoContinuar");
+const ContScreen02=document.getElementById("Screen02");
+const BtnSalir=document.getElementById("Salir");
+const TxtGuia=document.getElementById("Guia");
 
 BtnAdmin.addEventListener('click', () => {
-    
+    MostrarScreen();
 });
 BtnDeportes.addEventListener('click', ()=> {
-    
+    MostrarScreen();
 });
 BtnSalud.addEventListener('click',() => {
-    
+    MostrarScreen();
 });
+
+BtnImprimirOrden.addEventListener('click', () => {
+    CambiarScreen();
+});
+BtnSalir.addEventListener('click', ()=>{
+
+});
+
+function MostrarScreen(){
+    ContScreen01.style.visibility="visible";
+}
+function CambiarScreen(){
+    ContScreen01.style.visibility="hidden";
+    ContScreen02.style.visibility="visible";
+}
